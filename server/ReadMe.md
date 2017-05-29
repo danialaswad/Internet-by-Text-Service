dca838159e3bfda49c27ad958197d2cff7a0a3eb
danial <azswad20@gmail.com>
2017-05-29 15:57:03 +0200

    PIAJ-56 First version of the application serveur

--- server/ReadMe.md
+++
@@ -1,6 +0,0 @@
-**Server ITS** \
-before running the server, to command below need to be executed (Note that node is required) \
-`npm install gsm2` \
-
-Once installed, execute the command below to launch the server. \
-`npm main.js`
---
+++ server/src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by danial on 29/05/2017.
+ */
+public class Main {
+
+    public static void main(String args[]){
+        WebSite w = new WebSite("https://fr.wikipedia.org/wiki/Vampire");
+        System.out.println(w.getWebSite());
+    }
+}
