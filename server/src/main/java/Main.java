c75fc541f7d29fd649d893e3ae992a91e4554719
Anasse Ghira <ga302022@etu.unice.fr>
2017-05-30 15:33:18 +0200

    PIAJ-55, PIAJ-53  changement de l'encoding lors de la compression + decompression

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,16 +1,18 @@
 import web.URLReader;
 import web.WebPageCleaner;
 
-/**https://blogserius.blogspot.fr
- * https://en.wikipedia.org/wiki/Vampire
- *http://www.nicematin.com
+import java.io.IOException;
+
+/**
+ * https://blogserius.blogspot.fr
+ * Created by danial on 29/05/2017.https://en.wikipedia.org/wiki/Vampire
+ * http://www.nicematin.com
  */
 public class Main {
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
 
         URLReader w = new URLReader("https://en.wikipedia.org/wiki/Vampire");
-
         System.out.println(new WebPageCleaner().cleanWebPage(w.fetchFile()));
     }
 }
