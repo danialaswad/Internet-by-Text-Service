c03504adb960fa25c4c02ced6ec30a4d4bd5f9f8
AndoKarim <ando.karim@gmail.com>
2017-05-31 15:40:59 +0200

    Spinner lors du loading de la page

--- application/app/src/main/java/polytech/its/mobileapp/HomeActivity.java
+++ application/app/src/main/java/polytech/its/mobileapp/HomeActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsMessage;
@@ -15,6 +16,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class HomeActivity extends AppCompatActivity {
     public static final String PHONE_NUMBER = "+33628760946";
@@ -22,6 +24,8 @@ public class HomeActivity extends AppCompatActivity {
 
     private static WebView webArea;
     private static EditText URLArea;
+    private static ProgressBar mPbar;
+
 
     String url = "";
     static String smsContent = "";
@@ -37,12 +41,18 @@ public class HomeActivity extends AppCompatActivity {
 
         webArea = (WebView) findViewById(R.id.pageView);
         URLArea = (EditText) findViewById(R.id.urlEditor);
+        mPbar = (ProgressBar) findViewById(R.id.web_view_progress);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
 
         SmsUtility sr = new SmsUtility();
-        Cursor cursor = sr.getMessages(HomeActivity.this);
-        displaySms(cursor);
+
+        //Lire et afficher les SMS déjà présents sur le téléphone. A améliorer pour le systeme de cache? s
+        /*Cursor cursor = sr.getMessages(HomeActivity.this);
+        displaySms(cursor);*/
+
+        setTextViewAccueil();
+
 
         webArea.setWebViewClient(new WebViewClient() {
             @Override
@@ -51,11 +61,25 @@ public class HomeActivity extends AppCompatActivity {
                 clearViewAndSend(url);
                 return true;
             }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                mPbar.setVisibility(View.VISIBLE);
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                mPbar.setVisibility(View.GONE);
+            }
         });
+    }
+
+    private void setTextViewAccueil() {
+        webArea.loadData("<h1>Bienvenue sur ITS</h1><p>Entrez l'URL dans la barre ci-dessus et soyez patients :) </p>", "text/html", "UTF-8");
     }
 
     /**
      * Nettoyage de la webview afin d'accueillir le futur site
+     *
      * @param url
      */
     private void clearViewAndSend(String url) {
@@ -66,6 +90,7 @@ public class HomeActivity extends AppCompatActivity {
 
     /**
      * Récupération des SMS et vérification qu'il correspond au numéro du serveur
+     *
      * @param cursor
      */
     private void displaySms(Cursor cursor) {
@@ -88,6 +113,7 @@ public class HomeActivity extends AppCompatActivity {
 
     /**
      * Callback du bouton d'envoi
+     *
      * @param view
      */
 
@@ -113,7 +139,7 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-    public static class SmsListener extends BroadcastReceiver {
+    public static class     SmsListener extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             Bundle intentExtras = intent.getExtras();
