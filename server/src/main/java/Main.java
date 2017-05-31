c7607f47e2d1cbfe6aae807d718654ca992faa44
Anasse Ghira <ga302022@etu.unice.fr>
2017-05-31 15:45:47 +0200

    PIAJ-53 PIAJ55

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import compression.ZLibCompression;
 import engine.SmsServer;
 import org.smslib.SMSLibException;
 import web.URLReader;
@@ -14,19 +15,17 @@ import java.io.IOException;
  */
 public class Main {
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws InterruptedException, SMSLibException, IOException {
         //AIzaSyCPwDK5HD1C1kKJSLhLoRquom6mwv9Ydhs
-        URLReader w = new URLReader("http://anasghira.com/lorem");
+        //URLReader w = new URLReader("http://anasghira.com/lorem");
         //new WebPageCleaner().cleanWebPage(w.fetchFile());
         //Document document = w.fetchFile();
-        System.out.println(new WebPageCleaner().cleanWebPage(w.fetchFile(), w.getUrlString()));
+        //System.out.println(new WebPageCleaner().cleanWebPage(w.fetchFile(), w.getUrlString()));
         //testSmsServer();
     }
 
     public static void testSmsServer() throws InterruptedException, SMSLibException, IOException {
 
-        /*URLReader w = new URLReader("https://en.wikipedia.org/wiki/Vampire");
-        System.out.println(new WebPageCleaner().cleanWebPage(w.fetchFile()));*/
         SmsServer app = new SmsServer();
         try
         {
---
+++ server/src/main/java/compression/ZLibCompression.java
@@ -0,0 +1,47 @@
+package compression;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.*;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.InflaterInputStream;
+/**
+ * ZLibCompression
+ *
+ * @author Anasse GHIRA
+ * @version 1.0
+ */
+public class ZLibCompression {
+
+    public static String compressToBase64(String entry, String encoding) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            OutputStream out = new DeflaterOutputStream(baos);
+            out.write(entry.getBytes(encoding));
+            out.close();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        byte[] b64 = Base64.encodeBase64(baos.toByteArray());
+        return new String(b64);
+    }
+
+    public static String decompressFromBase64(String entry, String encoding) throws UnsupportedEncodingException {
+
+        byte[] dec = Base64.decodeBase64(entry.getBytes(encoding));
+        InputStream in = new InflaterInputStream(new ByteArrayInputStream(dec));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            byte[] buffer = new byte[8192];
+            int len;
+            while ((len = in.read(buffer)) > 0)
+                baos.write(buffer, 0, len);
+            return new String(baos.toByteArray(), encoding);
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
+}
+
+
+
