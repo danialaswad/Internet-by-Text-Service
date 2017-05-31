ebf3f0269ed848b0ea9dc3cf0ca8c2692186ebb9
Anasse Ghira <ga302022@etu.unice.fr>
2017-05-31 15:25:05 +0200

    PIAJ-74 renvoie du site demandé par l'app mobile (HTML)

--- server/src/main/java/engine/SmsServer.java
+++ server/src/main/java/engine/SmsServer.java
@@ -3,6 +3,8 @@ package engine;
 
 import org.smslib.*;
 import org.smslib.modem.SerialModemGateway;
+import web.URLReader;
+import web.WebPageCleaner;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -39,12 +41,28 @@ public class SmsServer {
         System.out.println("  Signal Level: " + gateway.getSignalLevel() + " dBm");
         System.out.println("  Battery Level: " + gateway.getBatteryLevel() + "%");
         System.out.println();
+        /*String msgBody = "<h1>HTML Ipsum Presents</h1>\n" +
+                "\n" +
+                "<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href=\"http://google.fr\" title=\"Dracula (1958 film)\">Dracula</a> in turpis pulvinar facilisis. Ut felis.</p>\n" +
+                "\n" +
+                "<h2>Header Level 2</h2>";
+        //TODO decouper le string
+        sendMessage("+33666360803",msgBody);*/
         ArrayList<InboundMessage> msgList = new ArrayList<InboundMessage>();
         while(true){
             Service.getInstance().readMessages(msgList, InboundMessage.MessageClasses.ALL);
             for (InboundMessage msg : msgList){
                 System.out.println(msg);
-                sendMessage("+"+msg.getOriginator(),"J'ai recu ton msg !");
+                //msg.getText();
+                URLReader w = new URLReader(msg.getText());
+                String msgBody = new WebPageCleaner().cleanWebPage(w.fetchFile(), w.getUrlString());
+                /*String msgBody = "<h1>HTML Ipsum Presents</h1>\n" +
+                        "\n" +
+                        "<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href=\"#\">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>\n" +
+                        "\n" +
+                        "<h2>Header Level 2</h2>";*/
+                //TODO decouper le string
+                sendMessage("+"+msg.getOriginator(),msgBody);
                 gateway.deleteMessage(msg);
             }
 
@@ -56,6 +74,7 @@ public class SmsServer {
     private void sendMessage(String to, String body) throws InterruptedException, TimeoutException, GatewayException, IOException {
         OutboundMessage msg = new OutboundMessage(to, body);
         Service.getInstance().sendMessage(msg);
+        System.out.println(msg);
     }
 
     public class InboundNotification implements IInboundMessageNotification
