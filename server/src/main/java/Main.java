da97e85d6d57e063992907c8d9eb05a5be5b7900
Anasse Ghira <ga302022@etu.unice.fr>
2017-05-31 13:25:00 +0200

    PIAJ-65, PIAJ-69. Renvoie de SMS lors de la reception d'un SMS

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,4 +1,5 @@
-import org.jsoup.nodes.Document;
+import engine.SmsServer;
+import org.smslib.SMSLibException;
 import web.URLReader;
 import web.WebPageCleaner;
 
@@ -16,6 +17,24 @@ public class Main {
         URLReader w = new URLReader("https://en.wikipedia.org/wiki/Vampire");
         //new WebPageCleaner().cleanWebPage(w.fetchFile());
         //Document document = w.fetchFile();
-        System.out.println(new WebPageCleaner().cleanWebPage(w.fetchFile(),w.getUrlString()));
+        System.out.println(new WebPageCleaner().cleanWebPage(w.fetchFile(), w.getUrlString()));
+    }
+
+    public void testSmsServer() throws InterruptedException, SMSLibException, IOException {
+
+        /*URLReader w = new URLReader("https://en.wikipedia.org/wiki/Vampire");
+        System.out.println(new WebPageCleaner().cleanWebPage(w.fetchFile()));*/
+        SmsServer app = new SmsServer();
+        try
+        {
+            app.run();
+
+        }
+        catch (Exception e)
+        {
+            app.stop();
+            e.printStackTrace();
+        }
+        //app.stop();
     }
 }
