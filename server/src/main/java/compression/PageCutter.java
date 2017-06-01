90437e1b1e500f3509563a11f2ca6b5b08ac6275
Antoine Rabenandrasana <antoine.rabenandrasana@etu.unice.fr>
2017-06-01 09:06:12 +0200

    PIAJ-54 On redemmare le découpage à zero

--- server/src/main/java/compression/PageCutter.java
+++ server/src/main/java/compression/PageCutter.java
@@ -9,22 +9,19 @@ import org.jsoup.select.Elements;
 import java.util.ArrayList;
 import java.util.Stack;
 
-
+//juste pac couper entre deux "<>"
+//recuperer tout jusqu 'au debut de la balise + la balise : doc.outerHtml.substring(0,mabalise.outterHtml.length)
 /**
  * Created by Antoine on 30/05/2017.
  */
 public class PageCutter {
 
     private Document page;
-    private Elements bodyChildren;
-    private int currentIndice;
-    private int size;
+    private int currentSize;
     private ArrayList<String> chunkList;
 
-    /*private Element currentElement;
-    private Stack<Element> elementStack;*/
 
-    private final static int SIZE_CHUNK = 3000;
+    private final static int SIZE_CHUNK = 9000;
 
     public PageCutter(String pageString){
         page=  Jsoup.parse(pageString);
@@ -36,51 +33,16 @@ public class PageCutter {
         elementStack = new Stack<>();*/
     }
 
-    /*
-    Algo poussé: TODO
-    Ini dans la pile mettre html,header,body
-    Element = position de départ
-    (i)Si en ajoutant le  prochain Element on ne depasse pas taille max
-            on l'ajoute on passe au suivant
-       Sinon
-            Si cet élément à des éléments à l'intérieur
-                On enregistre l'Element OU On enregistre le nom de la balise (pour fermer et pour le prochain paquet) dans une Stack
-                mdofication taille max
-                (i) sur les éléments à l'intérieur de celui-ci
-            Sinon
-                 On
-            on enregistre nouvelle position de départ
-            on entoure le contenu par ce qu'il y a en sommet de pile
 
-     Algo simple : (VERSION ACTUELLE)
-      on regarde juste si le prochain de body prend pas trop de place pour l'ajouter sinon on ferme avec les balises body header et html et on envoie
-
-     */
     private String nextPackage(){
-        /*size=0;
-        return write(currentElement);*/
-        String result="";
-        size+=surroundedByTag(page.select("body").first(),"").length();
-        while (true){
-            if (bodyChildren.size()>currentIndice) {
-                Element current = bodyChildren.get(currentIndice);
-                size+=current.outerHtml().length();
-                if(size<=SIZE_CHUNK) {
-                    result +=current.outerHtml();
-                    currentIndice++;
-                }else{break;}
-            }else {
-                break;
-            }
-        }
-        size=0;
-        return surroundedByTag(page.select("body").first(),result);
+        return null;
+    }
+
+    public String cut(){
+        return null;
     }
 
     public ArrayList<String> getChunkList(){
-        while (bodyChildren.size()>currentIndice){
-            chunkList.add(nextPackage());
-        }
         return chunkList;
     }
 
@@ -97,35 +59,9 @@ public class PageCutter {
         return openTag+htmlString+closeTag;
     }
 
-        /*private String write(Element element){
-        elementStack.add(element);
-        String result="";
-
-        if(size+element.outerHtml().length()<=SIZE_CHUNK){*/
-            /*Si en ajoutant le prochain element je ne dépasse pas la taille max*/
-            /*elementStack.pop();
-            size+=element.outerHtml().length();
-            if (element.nextElementSibling()==null){
-                return element.outerHtml();
-            }else {
-                return element.outerHtml() + write(element.nextElementSibling());
-            }
-        }else{*/
-            /* je ne peux pas ajouter l'élément car trop gros*/
-           /*if (!element.children().isEmpty()){*/
-               /*Si cet élément à des éléments à l'intérieur*/
-
-    //modification de la taille pour anticiper quand on devra refermer avec surroundedByTag
-               /*size+=5+(element.tagName().length()*2); // pour anticiper <tagName> </tagName>
-               if (!(element.attributes().size()==0))
-                   size+=element.attributes().toString().length(); //j'ajoute la longueur de l'attribut
-               //while (size <=SIZE_CHUNK) ??
-               result = write(element.children().first()); //write les éléments à l'intérieur de celui-ci
-           }
-
-           //enregistre nouvelle position de départ
-           currentElement=element;
-            return surroundedByTag(elementStack.pop(),result);
-        }
-    }*/
+    public void setPage(String pageString){
+        page=  Jsoup.parse(pageString);
+        chunkList.clear();
+    }
+    
 }
