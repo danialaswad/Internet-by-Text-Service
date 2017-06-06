6e35747ef63865fac6a6c02bfecda656af50c7bc
danial <azswad20@gmail.com>
2017-06-06 15:04:14 +0200

    PIAJ-85 refactoring the twitter functionality

--- server/src/main/java/twitter/TwitterManager.java
+++ server/src/main/java/twitter/TwitterManager.java
@@ -1,6 +1,8 @@
 package twitter;
 
 import database.ITSDatabase;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 
 /**
@@ -8,8 +10,17 @@ import twitter4j.auth.AccessToken;
  */
 public class TwitterManager {
 
+    private static final String APIKEY = "pIi6kVnGy12GFjECWELC8R4VK";
+    private static final String APISECRET = "WsCcYqlpJJ15DaRsKd7AnviQUEEn9Rd1PugDt6uFcdUvCz2Fxa";
+
     private ITSDatabase database = ITSDatabase.instance();
 
+    private Twitter twitter;
+
+    public TwitterManager(){
+        twitter = TwitterFactory.getSingleton();
+        twitter.setOAuthConsumer(APIKEY,APISECRET);
+    }
 
     public void configureAccount(String token, String tokenSecret, String id){
         AccessToken accessToken = new AccessToken(token, tokenSecret,Long.parseLong(id));
--- server/src/main/java/twitter/TwitterTimeline.java
+++ server/src/main/java/twitter/TwitterTimeline.java
@@ -1,7 +1,14 @@
 package twitter;
 
+import twitter4j.Twitter;
+import twitter4j.auth.AccessToken;
+
 /**
  * Created by danial on 06/06/2017.
  */
 public class TwitterTimeline {
+
+    public String userTimeline(AccessToken accessToken){
+        return "";
+    }
 }
