171807f4018caf404e2967ef610306fc5eb69c39
danial <azswad20@gmail.com>
2017-06-06 10:41:06 +0200

    code refactoring

---
+++ server/src/main/java/engine/SmsCommand.java
@@ -0,0 +1,52 @@
+package engine;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SmsCommand {
+
+    private PageManager pageManager;
+    private Map<String, Method> commands;
+
+    public SmsCommand(){
+        pageManager = new PageManager();
+        commands = initCommand();
+    }
+
+    private Map<String, Method> initCommand(){
+        Map<String, Method> map = new HashMap<>();
+        Method[] methods = this.getClass().getDeclaredMethods();
+        for (Method m : methods){
+            map.put(m.getName().toLowerCase(),m);
+        }
+        return map;
+    }
+
+    public String process(String cmd, String data){
+        if (!commands.containsKey(cmd.toLowerCase())){ return error(); }
+        Object o = null;
+        Method m;
+        try {
+            m = this.getClass().getMethod(commands.get(cmd.toLowerCase()).getName(),commands.get(cmd.toLowerCase()).getParameterTypes());
+            o = m.invoke(this, data);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            return error();
+        }
+        return (String) o;
+    }
+
+    public String get(String data){
+        return pageManager.getWebpage(data);
+    }
+
+    public String next(String data){
+         return pageManager.nexWebPage(data);
+    }
+
+    public String error(){
+        return  "<h2>Mauvaise commande</h2>";
+    }
+}
