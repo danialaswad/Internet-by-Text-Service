5bc1e55c83e89c1743d93c2b394aa7581bdb1638
AndoKarim <ando.karim@gmail.com>
2017-06-07 11:57:16 +0200

    PIAJ-96 Première version du popup d'envoi des tweets

--- application/app/src/main/java/polytech/its/mobileapp/TwitterFragment.java
+++ application/app/src/main/java/polytech/its/mobileapp/TwitterFragment.java
@@ -1,9 +1,11 @@
 package polytech.its.mobileapp;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.text.Editable;
@@ -83,12 +85,8 @@ public class TwitterFragment extends Fragment {
         sendTweet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                        getActivity());
-                LayoutInflater inflater = getActivity().getLayoutInflater();
-
-                alertDialogBuilder.setView(inflater.inflate(R.layout.dialog_tweet, null));
-                alertDialogBuilder.create();
+                CustomDialog dialog = new CustomDialog();
+                dialog.show(getFragmentManager(), "dialog");
 
 //        TextWatcher mTextEditorWatcher = new TextWatcher() {
 //            public void beforeTextChanged(CharSequence s, int start, int count,
