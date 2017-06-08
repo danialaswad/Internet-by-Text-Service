de3323782f31250ef8e0423613a3f7ac6eb3e877
danial <azswad20@gmail.com>
2017-06-08 12:38:44 +0200

    PIAJ-102 implementation du logger sur le serveur

--- server/src/main/java/engine/SmsServer.java
+++ server/src/main/java/engine/SmsServer.java
@@ -3,6 +3,7 @@ package engine;
 
 import compression.ZLibCompression;
 import database.ITSDatabase;
+import org.apache.log4j.Logger;
 import org.smslib.*;
 import org.smslib.modem.SerialModemGateway;
 
@@ -18,18 +19,18 @@ public class SmsServer  implements Runnable {
     private SmsCommand smsCommand;
     private Boolean shutdown = false;
 
+    private static final Logger LOG = Logger.getLogger(SmsServer.class);
+
+    private static final String DBFILE = "database.txt";
+
     private ITSDatabase database = ITSDatabase.instance();
 
     public SmsServer(String pin, String smscNumber, String comPort) throws GatewayException {
-        /*OutboundNotification outboundNotification = new OutboundNotification();
-        InboundNotification inboundNotification = new InboundNotification();*/
         gateway = new SerialModemGateway("modem.com9", comPort,125000, "", "");
         gateway.setInbound(true);
         gateway.setOutbound(true);
         gateway.setSimPin(pin);
         gateway.setSmscNumber(smscNumber);
-        //Service.getInstance().setOutboundMessageNotification(outboundNotification);
-        //Service.getInstance().setInboundMessageNotification(inboundNotification);
         Service.getInstance().addGateway(gateway);
     }
 
@@ -39,23 +40,15 @@ public class SmsServer  implements Runnable {
             readDatabase();
             smsCommand = new SmsCommand();
             Service.getInstance().startService();
-            System.out.println();
-            System.out.println("Modem Information:");
-            System.out.println("  Manufacturer: " + gateway.getManufacturer());
-            System.out.println("  Model: " + gateway.getModel());
-            System.out.println("  SIM IMSI: " + gateway.getImsi());
-            System.out.println("  Signal Level: " + gateway.getSignalLevel() + " dBm");
-            System.out.println("  Battery Level: " + gateway.getBatteryLevel() + "%");
-            System.out.println();
-            ArrayList<InboundMessage> msgList = new ArrayList<InboundMessage>();
+            logModemInfo();
+            ArrayList<InboundMessage> msgList = new ArrayList<>();
             while(!shutdown){
                 Service.getInstance().readMessages(msgList, InboundMessage.MessageClasses.ALL);
                 for (InboundMessage msg : msgList){
-                    String originNumber = "+"+msg.getOriginator();
-                    System.out.println(msg);
-                    String msgBody = smsCommand.process(msg.getText());
-                    String cryptedMsg = ZLibCompression.compressToBase64(msgBody,"UTF-8");
-                    sendMessage(originNumber,cryptedMsg);
+                    LOG.info("Sender : " +  msg.getOriginator());
+                    LOG.info("Message : " +  msg.getText());
+                    String cryptedMsg = ZLibCompression.compressToBase64(smsCommand.process(msg.getText()),"UTF-8");
+                    sendMessage("+"+msg.getOriginator(),cryptedMsg);
                     gateway.deleteMessage(msg);
                 }
                 msgList.clear();
@@ -67,7 +60,14 @@ public class SmsServer  implements Runnable {
         } catch (SMSLibException | InterruptedException | IOException e) {
             e.printStackTrace();
         }
+    }
 
+    private void logModemInfo() throws InterruptedException, TimeoutException, GatewayException, IOException {
+        LOG.info("Modem Manufacturer: " + gateway.getManufacturer());
+        LOG.info("Modem Model: " + gateway.getModel());
+        LOG.info("Modem SIM IMSI: " + gateway.getImsi());
+        LOG.info("Modem Signal Level: " + gateway.getSignalLevel() + " dBm");
+        LOG.info("Modem Battery Level: " + gateway.getBatteryLevel() + "%");
     }
 
 
@@ -75,7 +75,8 @@ public class SmsServer  implements Runnable {
     private void sendMessage(String to, String body) throws InterruptedException, TimeoutException, GatewayException, IOException {
         OutboundMessage msg = new OutboundMessage(to, body);
         Service.getInstance().sendMessage(msg);
-        System.out.println(msg);
+        LOG.info("Sender : " +  msg.getFrom());
+        LOG.info("Message : " +  msg.getText());
     }
 
     private void saveDatabase(){
@@ -83,20 +84,14 @@ public class SmsServer  implements Runnable {
         FileOutputStream fout = null;
         ObjectOutputStream oos = null;
         try {
-            fout = new FileOutputStream("database.txt");
+            fout = new FileOutputStream(DBFILE);
             oos = new ObjectOutputStream(fout);
-
             // sérialization de l'objet
             oos.writeObject(database) ;
-
-            System.out.println("Done");
-
+            LOG.info("Database stored in file " + DBFILE);
         } catch (Exception ex) {
-
             ex.printStackTrace();
-
         } finally {
-
             if (fout != null) {
                 try {
                     fout.close();
@@ -112,22 +107,19 @@ public class SmsServer  implements Runnable {
                     e.printStackTrace();
                 }
             }
-
-
-
         }
     }
 
-    public void readDatabase(){
+    private void readDatabase(){
 
         FileInputStream fin = null;
         ObjectInputStream ois = null;
         try {
-            fin = new FileInputStream("database.txt");
+            fin = new FileInputStream(DBFILE);
             ois = new ObjectInputStream(fin);
             database = (ITSDatabase) ois.readObject();
             ITSDatabase.setInstance(database);
-            System.out.println("test");
+            LOG.info("Database retrieve from file " + DBFILE);
 
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -150,7 +142,6 @@ public class SmsServer  implements Runnable {
             }
 
         }
-        System.out.println("hello");
     }
 
     public void stop() throws InterruptedException, SMSLibException, IOException {
